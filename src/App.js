59fb2d69bda85b6b528e05d747b1006dd4b3b7e6
zonar <424661@student.fontys.nl>
2021-01-18 00:56:26 +0100

    Images
    Finally got othe images to work was missing: ( ./ )

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ function App() {
    {
      title: "Good Mistake",
      artist: "Mr Little Jeans",
-     img_src: ".images/song1.jpg",
+     img_src: "../images/song-1.jpg",
      src: "./zmusic/good-mistake.mp3"
    },
     {
      title: "Apperition blues(live)",
      artist: "The Ragged Jubilee",
-     img_src: ".images/song-2.jpg",
-     src: "./zmusic/i-love-you-more-than-youll-ever-know.mp3"
+     img_src: "../images/song-2.jpg",
+     src: "./zmusic/apparition-blues.mp3"
    },
    {
     title: "I love you more than you'll ever know",
     artist: "Blood, Sweat and Tears",
-    img_src: ".images/song-3.jpg",
-    src: "./zmusic/laughing.mp3"
+    img_src: "../images/song-3.jpg",
+    src: "./zmusic/i-love-you-more-than-youll-ever-know.mp3"
   },
   {
     title: "Laughing",
     artist: "The Guess Who",
-    img_src: ".images/song-4.jpg",
-    src: "./zmusic/Apparittioin-blues.mp3"
+    img_src: "../images/song-4.jpg",
+    src: "./zmusic/laughing.mp3"
   }
  ]);
 
