12faaf975d9f4f768d35f56669b761994aab02dd
zonar <424661@student.fontys.nl>
2021-01-18 00:46:56 +0100

    Added music
    Added the music and functionality to the player

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import {useState} from 'react'
+import {useState, useEffect } from 'react'
 import Player from './components/Player';
 
 
 function App() {
- const [songs, setSongs] = useState([
+ const [songs] = useState([
    {
      title: "Good Mistake",
      artist: "Mr Little Jeans",
      img_src: ".images/song1.jpg",
-     src: "./zmusic/on-n-on.mp3"
+     src: "./zmusic/good-mistake.mp3"
    },
     {
      title: "Apperition blues(live)",
      artist: "The Ragged Jubilee",
      img_src: ".images/song-2.jpg",
-     src: "./zmusic/somebody-new.mp3"
+     src: "./zmusic/i-love-you-more-than-youll-ever-know.mp3"
    },
    {
     title: "I love you more than you'll ever know",
     artist: "Blood, Sweat and Tears",
     img_src: ".images/song-3.jpg",
-    src: "./zmusic/somebody-new.mp3"
+    src: "./zmusic/laughing.mp3"
   },
   {
     title: "Laughing",
     artist: "The Guess Who",
     img_src: ".images/song-4.jpg",
-    src: "./zmusic/somebody-new.mp3"
+    src: "./zmusic/Apparittioin-blues.mp3"
   }
  ]);
 
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  useEffect(() => {
+  setNextSongIndex(() => {
+  if (currentSongIndex + 1 > songs.length - 1) {
+    return 0;
+      } else {
+    return currentSongIndex + 1;
+       }
+   });
+   }, [currentSongIndex]);
+
   return (
     <div className="App">
      <Player
-     song={songs[currentSongIndex]}
-     nextSong={songs[nextSongIndex]}
+      currentSongIndex={currentSongIndex}
+      setCurrentSongIndex={setCurrentSongIndex}
+      nextSongIndex={nextSongIndex}
+      songs={songs}
      />
     </div>
   );
