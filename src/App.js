f0d3861f052c92bd66acd83d3d7fea7ef94df2b2
zonar <424661@student.fontys.nl>
2021-01-18 00:04:11 +0100

    Layout
    Changed the color and layout of the player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import Player from './components/Player';
-import PlayerDetails from './components/PlayerDetails'
+
 
 function App() {
  const [songs, setSongs] = useState([
